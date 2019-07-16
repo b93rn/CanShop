6c7679e6fe3b4569d5cdf4706a243d6f0096d750
Lenard Born <lborn@gmail.com>
2019-07-16 19:38:16 +0200

    Removed js backend.

--- backend/database.js
+++
@@ -1,7 +0,0 @@
-let Sequelize = require('sequelize');
-
-module.exports = new Sequelize('canshop', 'postgres', '36xv3', {
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432
-});
--- backend/models/helperFunctions.js
+++
@@ -1,9 +0,0 @@
-// Credit is stored in the db as cents, actually credit == db.credit / 100.
-module.exports.getCredit = function(credit) {
-    return (credit / 100).toFixed(2)
-}
-
-// Credit is stored in the db as cents, store credit as: actual credit * 100.
-module.exports.setCredit = function(credit) {
-    return (credit * 100).toFixed(2)
-}
